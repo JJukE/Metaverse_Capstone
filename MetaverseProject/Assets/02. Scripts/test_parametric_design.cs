01da098528a3cf13b0fc9232fd62b7645f43dfac
JJukE <psj9156@naver.com>
2022-03-18 12:53:05 +0900

    Parametric Design Practice
    Modified Asset Folders Structure, Added Parametric Design Scene, Implemented Practice

---
+++ MetaverseProject/Assets/02. Scripts/test_parametric_design.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_parametric_design : MonoBehaviour
+{
+    public Vector3 shell;
+    public GameObject shellObjCopy;
+
+    public void ParamChange()
+    {
+        shell = shellObjCopy.transform.localScale;
+        shell.x = 1.5f;
+        shellObjCopy.transform.localScale = shell;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
