f55afa1842e9a1319651572ebbdfa28d49c24629
YONGYONGYUN <15181417@seoultech.ac.kr>
2022-06-12 17:00:45 +0900

    0614VR

---
+++ MetaverseProject/Assets/02. Scripts/PLUSX.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngineInternal;
+
+
+
+public class PLUSX : MonoBehaviour
+{
+    public UnityEvent OnPressed;
+
+    public GameObject PLUSXS;
+    public GameObject GREENX;
+    public GameObject TABLEANI;
+    public bool XZERO;
+    // Start is called before the first frame update
+    void Start()
+    {
+       PLUSXS = GameObject.Find("MCT_ControlPanelMinusX");
+       GREENX = GameObject.Find("MCT_ControlPanelLightX");
+       TABLEANI = GameObject.Find("MCT_Table_1");
+
+        if (XZERO == true)
+        {
+            GREENX.GetComponent<Renderer>().material.color = Color.green;
+            TABLEANI.GetComponent<Animation>().Play("TABLEZERO");
+            
+        }
+            
+    }
+
+    // Update is called once per frame
+    private void Onpressed()
+    {
+        XZERO = true; 
+    }
+}
---
+++ MetaverseProject/Assets/02. Scripts/ZEROANI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZEROANI : MonoBehaviour
+{
+
+    public bool OnClick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        {
+            GetComponent<Animation>().Play("TABLEZERO");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
